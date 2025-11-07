afa73745738cf937dc46b54b0d34b03585103a0d
Md. Meshbaul Haque Tamim <2102028@che.buet.ac.bd>
2025-11-07 09:11:59 +0600

    feat: add secure routes for user account management including password change, current user retrieval, and avatar/cover image updates

--- Explorer/src/routes/user.route.js
+++ Explorer/src/routes/user.route.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,7 +30,18 @@ router.post("/login", loginUser);
 
 // --- -- - secure routes - -- --- //
 router.post("/logout", verifyJWT, logoutUser);
-router.post("/refresh-token", refreshAccessToken);
-// verifyJWT er kaj controller er moddhe kora hoyeche
+router.post("/refresh-token", refreshAccessToken); // verifyJWT er kaj controller er moddhe kora hoyeche
+router.post("/change-password", verifyJWT, changeCurrentUserPassword);
+router.get("/current-user", verifyJWT, getCurrentUser);
+router.patch("/update-account", verifyJWT, updateAccountDetails);
+router.patch("/avatar", verifyJWT, upload.single("avatar"), updateUserAvatar);
+router.patch(
+  "/coverImage",
+  verifyJWT,
+  upload.single("coverImage"),
+  updateUserCoverImage
+);
+router.get("/c/:userName", verifyJWT, getUserChannelProfile);
+router.get("/history", verifyJWT, getWatchHistory);
 
 export default router;
